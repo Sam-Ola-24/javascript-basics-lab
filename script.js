2af62c37a42b6633d13f1d4cbe4d291de25fac6b
Sam-Ola-24 <toye.akinola@gmail.com>
2024-09-20 17:04:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let studentName = 'Akinola Sam'
+let studentAge = 100
+let isEnrolled = true
+
+console.log(" Student Name: "+studentName + "\n Student Age: "+studentAge + " \n Enrollment Status: "+isEnrolled)
+
+console.log(" Student Age multiplied by 2 = "+studentAge * 2)
+console.log(" Student Age divided by 2 = "+studentAge / 2)
+console.log(" Student Age plus 2 = "+(studentAge + 2))
+console.log(" Student Age minus 2 = "+(studentAge - 2))
+
+console.log(" Student Age is greater than 2 = "+(studentAge > 2)) //true
+console.log(" Student Age is less than 2 = "+(studentAge < 2)) //false
+console.log(" Student Age = "+(studentAge)) //100
+console.log(" Student Age == 100 outputs"+(studentAge == 100)) //true
+console.log(" Student Age === '100' outputs "+(studentAge === "100")) //false
+console.log(" Student Age !== 100 outputs "+(studentAge !== 100)) //false
+
+console.log(" Student Age > 2 AND false = "+(studentAge > 2 && false)) //false
+console.log(" Student Age <  2 OR false = "+(studentAge > 2 || false)) //true
+console.log(" Student Age !(studentAge > 2 || false) = "+!(studentAge > 2 || false)) //false
+
+
+
+
+
